8675e844165f2727862516aa48dfd291fdbc1c6e
Maulik <rathodmaulikh@gmail.com>
2017-08-11 15:27:19 +0530

    MDN-Django Tutorial Part 4: Django admin site

---
+++ localLibrary/locallibrary/catalog/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from .models import Author, Book, BookInstance, Genre
+
+# Register your models here.
+
+# admin.site.register(Book)
+# admin.site.register(Author)
+# admin.site.register(Genre)
+# admin.site.register(BookInstance)
+
+# Define Admin Class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+# Register the Admin Class with the Associated Model
+admin.site.register(Author, AuthorAdmin)
+
+
+class BookInstanceInine(admin.TabularInline):
+    model = BookInstance
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInine]
+
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields':('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields':('status', 'due_back')
+        }),
+    )
