a935003cfec5015d156512a283aa0211c10ff4b3
Maulik <rathodmaulikh@gmail.com>
2017-08-14 15:42:17 +0530

    7: Sessions framework

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -13,14 +13,18 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count() # the 'all()' is implied by default
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     # Render the HTML template index.html with the data in the context variable
-    return render(request, 'index.html', context={'num_books':num_books, 'num_instances':num_instances, 'num_authors':num_authors, 'num_instances_available':num_instances_available})
+    return render(request, 'index.html', context={'num_books':num_books, 'num_instances':num_instances, 'num_authors':num_authors, 'num_instances_available':num_instances_available, 'num_visits':num_visits})
 
 
 # Class Based View
 class BookListView(generic.ListView):
     model = Book
-    paginate_by = 1
+    paginate_by = 5
 
     # context_object_name = 'my_book_list' # your own name for the list as a template variable
     # queryset = Book.objects.filter(title_icontains='war')[:5] # Get 5 books containing the title war
